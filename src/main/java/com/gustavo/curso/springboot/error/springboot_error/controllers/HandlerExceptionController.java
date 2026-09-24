9640556a2fd3aa0fcdea3b279a15269cb9a2c18c
GusDev071 <132627325+GusDev071@users.noreply.github.com>
2026-09-24 16:31:28 -0600

    Agregar controlador de excepciones para mejorar el mensaje de error 500

---
+++ src/main/java/com/gustavo/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -0,0 +1,26 @@
+package com.gustavo.curso.springboot.error.springboot_error.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.gustavo.curso.springboot.error.springboot_error.models.Error;
+
+@RestControllerAdvice 
+public class HandlerExceptionController {
+
+    @ExceptionHandler({ArithmeticException.class})
+    public ResponseEntity<?> divisionByZero(Exception ex){
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error division por cero!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error); 
+    }
+
+}
