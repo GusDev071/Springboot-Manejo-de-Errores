c53845128f51892653b6b6289a7bf245be6574fc
GusDev071 <132627325+GusDev071@users.noreply.github.com>
2026-09-24 16:46:27 -0600

    Agregar manejo de excepciones para rutas no encontradas y actualizar configuración de propiedades(Error 404)

--- src/main/java/com/gustavo/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/gustavo/curso/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.gustavo.curso.springboot.error.springboot_error.models.Error;
 
@@ -21,6 +22,17 @@ public class HandlerExceptionController {
         error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error); 
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Ruta de la api no encontrada");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
 
 }
